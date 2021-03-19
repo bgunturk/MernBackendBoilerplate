bded27c964fb91d876317f2ef3327fb48bab6b1e
bgunturk <bulent.gunturk@gmail.com>
2021-03-19 19:22:29 +0300

    backenddone

---
+++ server/config/dev.js
@@ -0,0 +1,3 @@
+module.exports = {
+    mongoURI:'mongodb+srv://bulentgunturk:1234512345Bg@cluster0.kshxj.mongodb.net/Cluster0?retryWrites=true&w=majority'
+}
---
+++ server/index.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
+const { User } = require('./models/user');
+const config = require('./config/key');
+
+mongoose
+.connect(config.mongoURI, {
+useUnifiedTopology: true,
+useNewUrlParser: true,
+})
+.then(() => console.log('DB Connected!'))
+.catch(err => {
+console.log(`${err.message}`);
+});
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+  });
+
+app.use(bodyParser.urlencoded({ extended:true }));
+app.use(bodyParser.json());
+app.use(cookieParser());
+
+app.post('/api/users/register' , function (req, res) {
+
+    const user= new User(req.body)
+    user.save((err, document) => {   
+        if(err) return res.json ({ success: false, err})
+    });
+    return res.status(200).json({
+        success: true,
+    });
+  });
+
+
+
+app.listen(5000);
